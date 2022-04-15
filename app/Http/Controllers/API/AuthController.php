3201cc48101438b340f21553b15e1aa9ca9eff83
dwijamahardika <dwijamahardika@gmail.com>
2022-04-15 21:05:46 +0700

    add login logout for owner

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,12 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use App\Models\tb_admin;
+use App\Models\tb_owner;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
 
-    public function login(Request $req)  {
+    public function loginAdmin(Request $req)  {
         $validator = $req -> validate ([
             'username_admin' => 'required|string',
             'password_admin' => 'required|string'
@@ -31,6 +32,27 @@ class AuthController extends Controller
             ], 400);
         }
         $token = $admin->createToken("secret") -> plainTextToken;
+        return response([
+            'message' => 'success to login',
+            'token' => $token
+        ], 200);
+    }
+
+    public function loginOwner(Request $req)  {
+        $validator = $req -> validate ([
+            'username_owner' => 'required|string',
+            'password_owner' => 'required|string'
+        ]);
+
+        $owner = tb_owner::where('username_owner', $validator['username_owner']) -> first();
+
+        if(!$owner || !($owner->password_owner == $validator["password_owner"])) {
+            return response([
+                "message" => "username or password can't be found",
+            ], 400);
+        }
+
+        $token = $owner->createToken("secret") -> plainTextToken;
         return response([
             'message' => 'success to login',
             'token' => $token
